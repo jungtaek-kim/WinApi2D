dd85bf3b3b8911ee3d3eb579f84b0f647bef739d
정택 김 <kimhyun3907@gmail.com>
2022-02-18 14:30:51 +0900

    TimeManager 기본 틀 작성

---
+++ WinApi2D/CTimeManager.cpp
@@ -0,0 +1,30 @@
+#include "framework.h"
+#include "CTimeManager.h"
+
+CTimeManager::CTimeManager()
+{
+
+}
+
+CTimeManager::~CTimeManager()
+{
+
+}
+
+void CTimeManager::update()
+{
+}
+
+void CTimeManager::init()
+{
+}
+
+unsigned int CTimeManager::GetFPS()
+{
+	return 0;
+}
+
+double CTimeManager::GetDT()
+{
+	return 0.0;
+}
