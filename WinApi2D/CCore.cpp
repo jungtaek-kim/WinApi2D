48fd6f6ac0d80e5eeda8eaf5e4777c57f7570449
정택 김 <kimhyun3907@gmail.com>
2022-02-18 14:15:55 +0900

    Core render() Dubble Buffering 기존의 윈도우 DC에 직접 그리는 방식(블링크 발생)에서 임시 메모리에 그린 후 옮겨 그리는 작업으로 변경

--- WinApi2D/CCore.cpp
+++ WinApi2D/CCore.cpp
@@ -3,12 +3,18 @@
 
 CCore::CCore()
 {
-
+	// 게임 화면을 그리기 위한 DC 핸들값 초기화
+	m_hDC = 0;
+	m_hMemDC = 0;
+	m_hBMP = 0;
 }
 
 CCore::~CCore()
 {
-
+	// 게임 코어 종료 시점에 DC 핸들값 반납
+	ReleaseDC(hWnd, m_hDC);
+	DeleteObject(m_hMemDC);
+	DeleteObject(m_hBMP);
 }
 
 void CCore::update()
@@ -18,10 +24,24 @@ void CCore::update()
 
 void CCore::render()
 {
-	// 게임 정보를 토대로 그리기 작업 진행
+	// 게임 정보를 토대로 memDC에 그리기 작업 진행
+	
+
+	// memDC에 그린 작업을 다시 윈도우 DC로 복사
+	BitBlt(m_hDC, 0, 0, WINSIZEX, WINSIZEY, m_hMemDC, 0, 0, SRCCOPY);
 }
 
 void CCore::init()
 {
 	// 게임 초기화 작업 진행
+
+	// 게임 윈도우의 DC 핸들값 가져오기
+	m_hDC = GetDC(hWnd);
+
+	// 더블 버퍼링의 메모리 DC와 비트맵 생성
+	m_hMemDC = CreateCompatibleDC(m_hDC);
+	m_hBMP = CreateCompatibleBitmap(m_hDC, WINSIZEX, WINSIZEY);
+
+	HBITMAP hOldBitmap = (HBITMAP)SelectObject(m_hMemDC, m_hBMP);
+	DeleteObject(hOldBitmap);
 }
