dcd3967655c9f8fddf5c724bbc7a3c33cb67f2d1
jungtaek-kim <kimhyun3907@gmail.com>
2022-02-22 01:59:41 +0900

    KeyManager 구현

--- WinApi2D/CKeyManager.cpp
+++ WinApi2D/CKeyManager.cpp
@@ -3,7 +3,12 @@
 
 CKeyManager::CKeyManager()
 {
-
+	// 키 상태를 모두 눌리지 않은 상태로 초기화
+	for (int key = 0; key < VKEY_SIZE; key++)
+	{
+		m_arrPrevKey[key] = false;
+		m_arrCurKey[key] = false;
+	}
 }
 
 CKeyManager::~CKeyManager()
@@ -13,6 +18,19 @@ CKeyManager::~CKeyManager()
 
 void CKeyManager::update()
 {
+	// 모든 키 사이즈만큼 반복하며 입력상태를 확인
+	for (int key = 0; key < VKEY_SIZE; key++)
+	{
+		m_arrPrevKey[key] = m_arrCurKey[key];
+		if (GetAsyncKeyState(key) & 0x8000)
+		{
+			m_arrCurKey[key] = true;
+		}
+		else
+		{
+			m_arrPrevKey[key] = false;
+		}
+	}
 }
 
 void CKeyManager::init()
@@ -21,15 +39,15 @@ void CKeyManager::init()
 
 bool CKeyManager::GetButton(const int key)
 {
-	return false;
+	return (true == m_arrCurKey[key] && true == m_arrPrevKey[key]);
 }
 
 bool CKeyManager::GetButtonUP(const int key)
 {
-	return false;
+	return (false == m_arrCurKey[key] && true == m_arrPrevKey[key]);
 }
 
 bool CKeyManager::GetButtonDOWN(const int key)
 {
-	return false;
+	return (true == m_arrCurKey[key] && false == m_arrPrevKey[key]);
 }
