d2d6e98c1d2c58f9bc4aa8529638e495e43025a0
jungtaek-kim <kimhyun3907@gmail.com>
2022-03-08 05:20:38 +0900

    ButtonUI 함수포인터로 기능 구현

--- WinApi2D/CButtonUI.cpp
+++ WinApi2D/CButtonUI.cpp
@@ -4,6 +4,9 @@
 CButtonUI::CButtonUI()
 	: CUI(false)
 {
+	m_pFunc = nullptr;
+	m_pParam1 = 0;
+	m_pParam2 = 0;
 }
 
 CButtonUI::~CButtonUI()
@@ -25,4 +28,19 @@ void CButtonUI::MouseLbtnDown()
 
 void CButtonUI::MouseLbtnUp()
 {
+}
+
+void CButtonUI::MouseLbtnClicked()
+{
+	if (nullptr != m_pFunc)
+	{
+		m_pFunc(m_pParam1, m_pParam2);
+	}
+}
+
+void CButtonUI::SetClickedCallBack(BTN_FUNC pFunc, DWORD_PTR param1, DWORD_PTR param2)
+{
+	m_pFunc = pFunc;
+	m_pParam1 = param1;
+	m_pParam2 = param2;
 }
--- WinApi2D/CButtonUI.h
+++ WinApi2D/CButtonUI.h
@@ -1,7 +1,16 @@
 #pragma once
 #include "CUI.h"
+
+// 함수포인터를 위한 타입정의
+typedef void(*BTN_FUNC) (DWORD_PTR, DWORD_PTR);
+
 class CButtonUI : public CUI
 {
+private:
+	BTN_FUNC m_pFunc;
+	DWORD_PTR m_pParam1;
+	DWORD_PTR m_pParam2;
+
 public:
 	CButtonUI();
 	~CButtonUI();
@@ -11,5 +20,8 @@ public:
 	virtual void MouseOn();
 	virtual void MouseLbtnDown();
 	virtual void MouseLbtnUp();
+	virtual void MouseLbtnClicked();
+
+	void SetClickedCallBack(BTN_FUNC pFunc, DWORD_PTR param1, DWORD_PTR param2);
 };
 
