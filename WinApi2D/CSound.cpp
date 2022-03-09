81e15215f18fbf4c9e6b114beecbaf6d6aa7fe6d
jungtaek-kim <kimhyun3907@gmail.com>
2022-03-10 05:00:43 +0900

    Sound 클래스 테스트 외부소스 사용

--- WinApi2D/CSound.cpp
+++ WinApi2D/CSound.cpp
@@ -1,2 +1,51 @@
 #include "framework.h"
 #include "CSound.h"
+
+CSound::CSound()
+{
+	system = nullptr;
+	sound = nullptr;
+	channel = nullptr;
+}
+
+CSound::~CSound()
+{
+	Stop();
+}
+
+void CSound::Load(const wstring& strFilePath)
+{
+	FMOD_RESULT ret;
+	char str[255];
+	wcstombs_s(nullptr, str, strFilePath.c_str(), 255);
+
+	ret = FMOD::System_Create(&system);
+	assert(system);
+	ret = system->init(32, FMOD_INIT_NORMAL, nullptr);
+	ret = system->createSound(str, FMOD_DEFAULT, 0, &sound);
+	assert(sound);
+}
+
+void CSound::Play()
+{
+	system->playSound(sound, 0, false, &channel);
+	assert(channel);
+}
+
+void CSound::Pause()
+{
+	if (nullptr != channel)
+		channel->setPaused(true);
+}
+
+void CSound::Stop()
+{
+	if (nullptr != channel)
+		channel->stop();
+}
+
+void CSound::Resume()
+{
+	if (nullptr != channel)
+		channel->setPaused(true);
+}
--- WinApi2D/CSound.h
+++ WinApi2D/CSound.h
@@ -1,7 +1,27 @@
 #pragma once
+#include "FMOD/inc/fmod.hpp"
 #include "CResource.h"
-class CSound :
-    public CResource
+#pragma comment(lib,"FMOD/lib/fmod_vc.lib")
+
+using namespace FMOD;
+
+class CSound : public CResource
 {
+private:
+
+	System*		system;
+	Sound*		sound;
+	Channel*	channel;
+
+public:
+	CSound();
+	~CSound();
+
+	void Play();
+	void Stop();
+	void Pause();
+	void Resume();
+
+	void Load(const wstring& strRelativePath);
 };
 
